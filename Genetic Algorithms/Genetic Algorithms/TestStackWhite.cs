54b70d0df4e76fe13dd07866f7338ba7c88b1117
Aleksander Kulon <virtroy@gmail.com>
2019-05-09 22:27:16 +0200

    Adding Empty test Class

---
+++ Genetic Algorithms/Genetic Algorithms/TestStackWhite.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.White;
+using TestStack.White.WindowsAPI;
+
+namespace Genetic_Algorithms
+{
+    class TestStackWhite
+    {
+    }
+}
